8235d5eb67c914e663e51e34e8f197ac7488b3c5
Lowell Bander <lowellbander@gmail.com>
2014-11-16 03:27:49 -0500

    does the queries

--- twython_test.py
+++ twython_test.py
@@ -5,26 +5,29 @@ from pprint import pprint
 APP_KEY = 'hvddwcOWke5tcwZ7fqsylEUrD'
 APP_SECRET = 'J0S2QdnMHa7dos3eX0xjho9rF8f0aw5OGsFAyiRzXMEBdF9Qxc'
 
-def do_query(terms):
+def do_query(query):
     twitter = Twython(APP_KEY, APP_SECRET)
-    query = " OR ".join(terms)
     query = urllib.quote_plus(query)
+    query = query[:140] # twitter queries limited to 140 chars
     print "query: " + query
-    return twitter.search(q=query)
+    return twitter.search(q=query, count=15)
+
+def print_results(results, fields):
+    for status in results['statuses']:
+        print "\n\n --------------------------------- \n\n"
+        for field in fields:
+            print field + ": ",
+            print status[field]
 
 def main():
 
-    #terms = ['I-40', 'I-85', 'I-440', 'I-540', 'US Route 1', 'US Route 15', 'US Route 64', 'US Route 70', 'US Route 264', 'US Route 401', 'US Route 501', 'NC147', 'NC540 ', 'car crash', 'car accident', 'car pile up', 'car collision']
+    terms = ['I-40', 'I-85', 'I-440', 'I-540', 'US Route 1', 'US Route 15', 'US Route 64', 'US Route 70', 'US Route 264', 'US Route 401', 'US Route 501', 'NC147', 'NC540 ', 'car crash', 'car accident', 'car collision', 'car pile up']
 
-    terms = ['blue', 'red']
+    fields = ['text', 'geo']
 
-    results = do_query(terms)
-
-    for status in results['statuses']:
-        print "\n\n --------------------------------- \n\n"
-        print "text:" + status['text']
-        print "geo:" + str(status['geo'])
-
+    for term in terms:
+        results = do_query(term)
+        print_results(results, fields)
 
 if __name__ == "__main__":
     main()
